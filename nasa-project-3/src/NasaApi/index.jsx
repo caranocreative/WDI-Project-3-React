0e7721e1c73005267b5b2976a5b84fb9c8cbb8a0
Laurie Carano <lauriecarano@gmail.com>
2018-08-09 15:37:47 -0600

    one more step

--- nasa-project-3/src/NasaApi/index.jsx
+++ nasa-project-3/src/NasaApi/index.jsx
@@ -30,21 +30,21 @@ class NasaApi extends Component {
     }
   }
   
-  addPicture = async (e) => {
+  addComment = async (e) => {
    //console.log("add new comment");
     e.preventDefault();
     // console.log(this.state.url);
     // console.log("ADDING A PICTURE");
     try {
       // const imageToCreate = {"imageUrl":  this.state.url};
-      const createdPicture = await fetch('http://localhost:9000/api/v1/pictures', {
+      const createdComment = await fetch('http://localhost:9000/api/v1/pictures', {
           method: 'POST',
           body: JSON.stringify(this.state),
           headers:{
             'Content-Type': 'application/json'
           }
         });
-        const data = await createdPicture.json()
+        const data = await createdComment.json()
         this.setState({
           id: data.data._id
         }) 
@@ -53,21 +53,16 @@ class NasaApi extends Component {
       console.log(err)
     }
   }
-  // handleSubmit = async (e) => {
-  //   e.preventDefault();
-  //   try {
-  //     const nasaApiData = await fetch('http://localhost:9000/api/v1/pictures', {
-  //       method: 'POST',
-  //         body: JSON.stringify(this.state.url),
-  //         headers:{
-  //           'Content-Type': 'application/json'
-  //         }
-  //     })
-  //   } catch (err) {
-  //     console.log(err)
-  //   }
-  // }
-  addNewComment = async (id, e) => {
+  
+  getComments = async () => {
+
+    const comments = await fetch('http://localhost:9000/api/v1/pictures/');
+    const commentsJson = await comments.json();
+    return commentsJson
+
+  }
+
+ editComment = async (id, e) => {
     console.log('inside function');
     e.preventDefault();
     try {
@@ -106,6 +101,8 @@ class NasaApi extends Component {
         explanation: data.explanation,
         date: data.date,
         title: data.title,
+        comment: data.comment,
+        id: data.id,
         
       });
     });
@@ -114,50 +111,66 @@ class NasaApi extends Component {
   
   //like the picture of the day works
   render(props) {   
-    let s = "";
-    for(let i = 0; i < this.state.comment.length; i++)  {
-      s += `
-      <form>
-        <textarea value={this.state.comment[i]}/>
-        <input type='submit' value='Edit Comment'/>
-        <input type='submit' value='Delete Comment'/>
-      </form>`
-   }
+  //   let s = "";
+  //   for(let i = 0; i < this.state.getComments.length; i++)  {
+  //     s += `
+  //     <form>
+  //       <textarea value={this.state.comment[i]}/>
+  //       <input type='submit' value='Edit Comment'/>
+  //       <input type='submit' value='Delete Comment'/>
+  //     </form>`
+  //  }
     
     return (
       <div className="mainContainer"> 
       <div className="row">
       <div className="side">
-      <div>
+        
+        <div>
         <div className="POD-main">
         <h1 className="POD-h1">Out of this World Space Fun</h1>
         <h3 className="POD-h3">NASA Picture of the Day!</h3>
         <iframe className="POD" src={this.state.url} alt=""></iframe>
         </div>
-        <h2>Like the Picture of the day</h2>
-        <form onSubmit={this.addPicture}>
-          <input type='hidden' name='url' value={this.state.url}/>
-          <input type='hidden' name='explanation' value={this.state.explanation}/>
-          <input type='hidden' name='title' value={this.state.title}/>
-          <input type='hidden' name='date' value={this.state.date}/>
-          <input type='submit' value='Like'/>
-        </form>
         
-        <h2>Add your comments</h2>
-        <form onSubmit={this.addNewComment}>                                                                                          
-          <textarea name='comment' onChange={this.handleFormChange} /> 
-          <br/>       
-          <input type='submit' value='Add Comment'/>
-          <div>
-          <br/>
-          </div>
-        </form>
-
+        {/* <div>
+          <h2>Like the Picture of the day</h2>
+          <form onSubmit={this.addPicture}>
+            <input type='hidden' name='url' value={this.state.url}/>
+            <input type='hidden' name='explanation' value={this.state.explanation}/>
+            <input type='hidden' name='title' value={this.state.title}/>
+            <input type='hidden' name='date' value={this.state.date}/>
+            <input type='submit' value='Like'/>
+          </form>
+        </div> */}
+        
+        <br/>
+        
+        <div>
+          <h2>Add your comments</h2>
+          <form onSubmit={this.addComment}>                                                                                          
+            <textarea name='comment' onChange={this.handleFormChange} /> 
+            <br/>       
+            <input type='submit' value='Add Comment'/>
+            <div>
+            <br/>
+            </div>
+          </form>
+        </div>
           
-          <h2>Review Comments</h2>
-          {s}     
-             
+          {/* <div>
+            <h2>Review Comments</h2>
+            {s}     
+          </div>  */}
          
+         <div>
+           <h2>COMMENTS</h2>
+          <div >
+
+          </div>
+        </div>
+
+
       </div>
       </div>
       </div>
